949534ce7abd6bde011666330671c9f348dae4fc
Dylan Young <dylan.young@colorado.edu>
2022-02-01 15:33:46 -0700

    Added versioning using setuptools_scm

--- src/thompson_pulsed/__init__.py
+++ src/thompson_pulsed/__init__.py
@@ -1,2 +1,11 @@
 # Import core functions and classes
 from .core import *
+
+# Import version number from pkg_resources
+# NOTE: this is apparently slow. Consider parsing from a _version.py instead
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
