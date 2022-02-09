bc9beef45d481bbb141e701016433795dd8b71f4
SYNC <thompsonjila@gmail.com>
2022-02-09 16:39:34 -0700

    Made watchdog script stable

--- tests/e3l_watchdog_test.py
+++ tests/e3l_watchdog_test.py
@@ -10,9 +10,9 @@ This is an example watchdog script for the three_level experiment.
 import sys
 sys.path.insert(0, '../src')
 
-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 import time
 import logging
@@ -48,7 +48,18 @@ class Custom_EventHandler(FileSystemEventHandler):
         if not event.is_directory:
             self.events.append(event)
             
-def watch_dir(path='.'):
+"""
+Curve fit
+"""
+gamma = 2*np.pi * 7.5 * 1e3
+def exponential(t, A, tau, C):
+    return( A*np.exp(-t/tau) + C )
+fit = lambda t,A,C: exponential(t, A, 1/gamma, C)
+
+"""
+Watchdog function
+"""            
+def watch_dir(path='.', keep_files=True):
     # Initialize experiment
     expt = e3l.Experiment(params)
     
@@ -76,19 +87,23 @@ def watch_dir(path='.'):
         plt.ion()
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        atom_trace = None
+        ax.set_xlim(0, 50)
+        ax.set_ylim(-500, 500)
+        ax.grid()
+        cav_probe = None
         
         # Loop indefinitely to watch for file creation
         print('Press Ctrl+C to stop watchdog observer')
-        while True:          
-            # Look for an modify-file event followed by a create-file event
-            # The create-file event implies the previous file is ready to read
-            event = None
-            for i in range(2, len(event_handler.events)+1):
-                if event_handler.events[-i].event_type == 'modified' \
-                    and event_handler.events[-(i-1)].event_type == 'created':
-                        event = event_handler.events[-i]
-                        break
+        while True:
+            # Look for the second most recent FileCreatedEvent
+            # Second most recent hopefully ensures it is completely loaded
+            event, created_events = None, 0
+            for i in range(1, len(event_handler.events)+1):
+                if event_handler.events[-i].event_type == 'created':
+                    created_events += 1
+                if created_events == 2:
+                    event = event_handler.events[-i]
+                    break
             if not event:
                 continue
             
@@ -99,23 +114,60 @@ def watch_dir(path='.'):
                 loaded_files.append(newest_file)
                 print(f'File loaded: {newest_file}')
                 
+                # Delete old file
+                if not keep_files:
+                    try:
+                        if len(loaded_files) > 2:
+                            os.remove(loaded_files[0])
+                            loaded_files = loaded_files[1:]
+                    except:
+                        if not os.path.exists(loaded_files[0]):
+                            loaded_files = loaded_files[1:]
+                
                 # Process enough data to display demodulated atom trace
                 expt.preprocess(n_shots = 1, load = 'newest')
-                shot_atom_demod = expt.data.demod_atom_trace()
-                t = shot_atom_demod.t
-                shot_atom_demod_avg = np.mean(shot_atom_demod.V, axis=0)
+                bin_times, cav_freqs = expt.data.track_cav_frequency_iq()
+                cav_freqs_mean = np.mean(cav_freqs, axis=0)
+                cav_freqs_stdmean = np.std(cav_freqs, axis=0) / np.sqrt(cav_freqs.shape[0])
+                
+                [pOpt, pCov] = curve_fit(fit,
+                                         bin_times, cav_freqs_mean,
+                                         sigma = cav_freqs_stdmean,
+                                         p0=[1,0], 
+                                         bounds=([0,-500e3],[np.inf, 500e3])               
+                    )
                 
                 # Update plot with new trace
-                if atom_trace:
-                    atom_trace.set_ydata(shot_atom_demod_avg)
-                else:
+                if not cav_probe:
                     # No plot yet. Create one
-                    atom_trace, = ax.plot(t, shot_atom_demod_avg)
+                    cav_probe = ax.errorbar(bin_times * 1e6, cav_freqs_mean * 1e-3,
+                                            cav_freqs_stdmean * 1e-3,
+                                            fmt='ok', alpha=0.2)
+                    cav_fit = ax.plot(bin_times * 1e6,
+                                      fit(bin_times, *pOpt) * 1e-3, 'k',
+                                      label=f'f0 = {round(pOpt[0]*1e-3)} +/- {round(np.sqrt(pCov[0,0])*1e-3)} kHz')
+                    ax.set(xlabel=r'Time ($\mu s$)', ylabel=r'Frequency (kHz)')
+                    ax.legend(loc='upper right')
+                else:
+                    cav_probe[0].remove()
+                    for line in cav_probe[1]:
+                        line.remove()
+                    for line in cav_probe[2]:
+                        line.remove()
+                    cav_probe = ax.errorbar(bin_times * 1e6, cav_freqs_mean * 1e-3, 
+                                            cav_freqs_stdmean * 1e-3,
+                                            fmt='ok', alpha=0.2)
+                    cav_fit[0].set(xdata=bin_times * 1e6,
+                                   ydata=fit(bin_times, *pOpt) * 1e-3,
+                                   label=f'f0 = {round(pOpt[0]*1e-3)} +/- {round(np.sqrt(pCov[0,0])*1e-3)} kHz')
+                    ax.legend(loc='upper right')
+                    
                 fig.canvas.draw()
                 fig.canvas.flush_events()
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+
     
 # TESTING ONLY: Remove package from path when done
 sys.path.remove('../src')
